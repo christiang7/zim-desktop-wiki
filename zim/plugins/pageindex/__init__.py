fb6a5e8dc7f76312d7627d135e1f2fa991ceb767
christiang7 <98482045+christiang7@users.noreply.github.com>
2024-04-15 23:20:43 +0200

    Update __init__.py

--- zim/plugins/pageindex/__init__.py
+++ zim/plugins/pageindex/__init__.py
@@ -400,7 +400,7 @@ class PageTreeView(BrowserTreeView):
 		self._autoexpanded = None
 		self._autoexpand = True
 		self._autocollapse = True
-		self._last_selected_path = None
+#		self._last_selected_path = None
 
 		column = Gtk.TreeViewColumn('_pages_')
 		column.set_expand(True)
@@ -476,10 +476,10 @@ class PageTreeView(BrowserTreeView):
 		mytreeiter = model.get_user_data(treeiter)
 		selected_path = self.get_selected_path()
 
-		if self._last_selected_path == selected_path:
-			return
-		self._last_selected_path = selected_path
-
+#		if self._last_selected_path == selected_path:
+#			return
+#		self._last_selected_path = selected_path
+#
 		if self._autocollapse:
 			self.restore_autoexpanded_path()
 
