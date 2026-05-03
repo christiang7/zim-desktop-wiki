015f0558202b787ca79a2ca9efcc0f56463f1be5
christiang7 <christian.g@mailbox.org>
2026-05-03 19:09:01 +0200

    fix image with parentheses

--- zim/formats/markdown.py
+++ zim/formats/markdown.py
@@ -148,7 +148,7 @@ class MarkdownParser(object):
 			| Rule(LINK, r'<([a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>', process=self.parse_autolink) # email autolink
 			| Rule(LINK, url_link_re, process=self.parse_url)
 			| Rule(LINK, r'\[\[(?!\[)(.*?\]*)\]\]', process=self.parse_wiki_link)
-			| Rule(IMAGE,   r'!\[([^\]]*)\]\(([^)]+)\)(\{[^}]*\})?', process=self.parse_image)
+			| Rule(IMAGE,   r'!\[([^\]]*)\]\((\S+)\)(\{[^}]*\})?', process=self.parse_image)
 			| Rule(LINK, r'\[([^\]]*)\]\((\S+)\)', process=self.parse_link)
 			| Rule(ANCHOR, r'\{\#(\w[\w-]*)\}', process=self.parse_anchor)
 			| Rule(TAG, r'(?<!\S)@\w+', process=self.parse_tag)
